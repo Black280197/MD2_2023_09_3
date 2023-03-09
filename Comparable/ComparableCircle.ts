9d34a48b00d9f71d94133bee7ff8c6c486af2bdb
Black280197 <121843405+Black280197@users.noreply.github.com>
2023-03-09 14:47:43 +0700

    Refactoring

---
+++ Comparable/ComparableCircle.ts
@@ -0,0 +1,12 @@
+import {Circle} from "./Circle";
+import {Comparable} from "./Circle";
+
+export class ComparableCircle extends Circle implements Comparable {
+    constructor(radius: number) {
+        super(radius);
+        }
+
+        compareTo(o: ComparableCircle): number {
+        if (this.getRadius() > )
+        }
+}
